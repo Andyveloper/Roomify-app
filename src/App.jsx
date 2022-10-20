3439ee5ba79af553036d5e66ad19bce6165a624b
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-20 12:59:31 -0500

    Create react app and install deps

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Button} from "@mui/material"
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="secondary">
+      Hello World
+        
+      </Button>
+    </div>
+  );
+}
+
+export default App;
