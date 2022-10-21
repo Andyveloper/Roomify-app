73c5afe97b40bf8d33fa5bb899e1149bff92fbf6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-21 17:06:44 +0100

    Add style to the corousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosNewSharpIcon from '@mui/icons-material/ArrowBackIosNewSharp';
 import responsive from './CarouselStyling';
+import './carousel.css';
 
 const CarouselComp = () => {
   const [pictures, setPictures] = useState([]);
@@ -14,59 +13,61 @@ const CarouselComp = () => {
     const apiKey = '30748294-b32c0b587bb743f77a631dffb';
     (async () => {
       const { data } = await axios.get(`https://pixabay.com/api/?key=${apiKey}`);
+      // const three = data.hits.map(())
       setPictures(data.hits);
     })();
   }, [pictures]);
 
-
-
   // { next, previous, goToSlide, ...rest }
-  const ButtonGroup = (array, currentSlide) => {
-    return (
-      <div className="carousel-button-group" style={{position: 'absolute'}}> 
-      {/* remember to give it position:absolute */}
-        <ArrowBackIosNewSharpIcon className={currentSlide === 0 ? 'disable' : ''} onClick={() => currentSlide - 1} />
-        {/* <ButtonTwo onClick={() => next()} /> */}
-        <ArrowForwardIosIcon onClick={() => currentSlide + 1} />
-      </div>
-    );
-  };
-
-
+  // const ButtonGroup = (array, currentSlide) => (
+  //   <div className="carousel-button-group">
+  //     {/* remember to give it position:absolute */}
+  //     <ArrowBackIosNewSharpIcon className={currentSlide === 0 ? 'disable' : ''} onClick={() => currentSlide - 1} />
+  //     {/* <ButtonTwo onClick={() => next()} /> */}
+  //     <ArrowForwardIosIcon onClick={() => currentSlide + 1} />
+  //   </div>
+  // );
 
   return (
-<>
-    <Carousel
-      responsive={responsive}
-      infinite
-      autoPlay="desktop"
-      autoPlaySpeed={4000}
-      keyBoardControl
-      transitionDuration={4000}
-      containerClass="carousel-container"
-      itemClass
-      centerMode
-      slidesToSlide={3}
-      arrows
-      arrows={false}
-      showDots={true}
-      renderButtonGroupOutside={true}
-      customButtonGroup={pictures.length ? <ButtonGroup /> : null}
-    >
-      { pictures.length
-        ? pictures.map((ind) => (
-          <React.Fragment key={ind.id}>
-          <NavLink to={ind.type}>
-            <div className="image-carousel">
-          <img src={ind.previewURL} alt={ind.type} />
-          </div>
-          </NavLink>
-          </React.Fragment>
-        ))
-        : <div>Loading</div>}
-    </Carousel>
-    </>  
-    );
+    <>
+      <Carousel
+        responsive={responsive}
+        infinite
+        autoPlay="desktop"
+        autoPlaySpeed={4000}
+        keyBoardControl
+        transitionDuration={500}
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        containerClass="carousel-container"
+        itemClass
+        centerMode
+        arrows
+        slidesToSlide={3}
+        showDots
+        renderArrowsWhenDisabled
+        // customButtonGroup={pictures.length ? <ButtonGroup /> : null}
+      >
+        { pictures.length
+          ? pictures.map((ind) => (
+            <React.Fragment key={ind.id}>
+              <NavLink to={ind.type}>
+                <div 
+                  className="image-carousel"
+                  style={{ width: '100%', height: '100%' }}
+                >
+                  <img
+                    style={{ width: '100%', height: '100%', cursor: 'pointer' }}
+                    src={ind.previewURL}
+                    alt={ind.type}
+                  />
+                </div>
+              </NavLink>
+            </React.Fragment>
+          ))
+          : <div>Loading</div>}
+      </Carousel>
+    </>
+  );
 };
 
 export default CarouselComp;
