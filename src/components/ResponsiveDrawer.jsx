1c4c7b9c88ccec8e6deccbbba7425d50d11f4344
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-20 22:17:58 -0500

    Create Responsive Drawer(Sidebar)
    Add required imports
    Create menu items object
    Create behavior functions
    Add .map method to rendering function

--- src/components/ResponsiveDrawer.jsx
+++ src/components/ResponsiveDrawer.jsx
@@ -1,22 +1,16 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
+import {
+  AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemButton,
+  ListItemText, ListItemIcon, Toolbar, Typography, SwipeableDrawer,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { SwipeableDrawer } from '@mui/material';
+import BedroomParentIcon from '@mui/icons-material/BedroomParent';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import NoMeetingRoomIcon from '@mui/icons-material/NoMeetingRoom';
+import BookOnlineIcon from '@mui/icons-material/BookOnline';
+import ClassIcon from '@mui/icons-material/Class';
+import HouseIcon from '@mui/icons-material/House';
 
 const drawerWidth = 240;
 
@@ -28,54 +22,89 @@ function ResponsiveDrawer(props) {
     setMobileOpen(!mobileOpen);
   };
 
-  const menuItems = {
-    user: ['Rooms', 'Reserve', 'My Reservations'],
-    admin: ['Create Room', 'Delete Room']
-  }
- 
+  const menuItems = [
+    {
+      text: 'Home',
+      icon: <HouseIcon />,
+      onClick: '/',
+    },
+    {
+      text: 'Rooms',
+      icon: <BedroomParentIcon />,
+      onClick: '/rooms',
+    },
+    {
+      text: 'Reserve',
+      icon: <BookOnlineIcon />,
+      onClick: '/reserve',
+    },
+    {
+      text: 'My Reserves',
+      icon: <ClassIcon />,
+      onClick: '/my-reserves',
+    },
+  ];
+  const adminItems = [
+    {
+      text: 'Create Room',
+      icon: <MeetingRoomIcon />,
+      onClick: '/create-rooms',
+    },
+    {
+      text: 'Delete Room',
+      icon: <NoMeetingRoomIcon />,
+      onClick: '/delete-room',
+    },
+  ];
 
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-      <Box p={2} width="200px" textAlign="left" role="presentation">
-       <Typography variant='h5' noWrap component="div">
-          Options
-        </Typography>
+        <Box p={2} width="200px" textAlign="left" role="presentation">
+          <Typography variant="h5" noWrap component="div">
+            Options
+          </Typography>
         </Box>
-        {menuItems.user.map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {menuItems.map((item) => {
+          const { text, icon, onClick } = item;
+          return (
+            <ListItem key={text} disablePadding>
+              <ListItemButton href={onClick}>
+                <ListItemIcon>
+                  {icon}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
       </List>
       <Divider />
       <List>
-        {menuItems.admin.length !== 0
-        ? 
-        <Box p={2} width="200px" textAlign="left" role="presentation">
-        <Typography variant='h5' noWrap component="div">
-          Admin Options 
-        </Typography>
-        </Box>
-        : <div></div>
-        }
-        {menuItems.admin.map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {adminItems.length !== 0
+          ? (
+            <Box p={2} width="200px" textAlign="left" role="presentation">
+              <Typography variant="h5" noWrap component="div">
+                Admin Options
+              </Typography>
+            </Box>
+          )
+          : <div />}
+        {adminItems.map((item) => {
+          const { text, icon, onClick } = item;
+          return (
+            <ListItem key={text} disablePadding>
+              <ListItemButton href={onClick}>
+                <ListItemIcon>
+                  {icon}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
       </List>
     </div>
   );
@@ -139,39 +168,6 @@ function ResponsiveDrawer(props) {
           {drawer}
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
     </Box>
   );
 }
@@ -181,7 +177,7 @@ ResponsiveDrawer.propTypes = {
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
    */
-  window: PropTypes.func,
+  window: PropTypes.func.isRequired,
 };
 
 export default ResponsiveDrawer;
