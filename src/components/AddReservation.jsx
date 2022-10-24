cd52013e830e8193e3e932fed3db4ec8fdeba2b5
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-24 23:43:23 +0100

    Set the form POST

--- src/components/AddReservation.jsx
+++ src/components/AddReservation.jsx
@@ -1,19 +1,42 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const AddReservation = () => {
   const data = useSelector((state) => state.reserveProperty);
-
+  const url = '';
   const [property, setProperty] = useState({});
-  const [cityName, setCityName] = useState('');
-  const [date, setDate] = useState('');
+
+  const [userReserve, setUserReserve] = useState({
+    property: data,
+    cityName: '',
+    date: '',
+  });
 
   const todayDate = new Date().toISOString().slice(0, 10);
+
+  const handleUserReserve = (e) => {
+    const newData = { ...userReserve };
+    newData[e.target.id] = e.target.value;
+    setUserReserve(newData);
+  };
 
   useEffect(() => {
     setProperty(data);
   }, [property]);
+
+  const submit = (e) => {
+    e.preventDefault();
+    axios.post(url, {
+      property: userReserve.property,
+      cityName: userReserve.cityName,
+      date: userReserve.date,
+    })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
 
   return (
     <div>
@@ -21,6 +44,7 @@ const AddReservation = () => {
       <div
         className="image-body"
         style={{ width: '200px', height: '200px' }}
+        id=""
       >
         <img
           src={property.largeImageURL}
@@ -29,7 +53,10 @@ const AddReservation = () => {
           style={{ width: '100%', height: '100%', cursor: 'pointer' }}
         />
       </div>
-      <form className="row g-3">
+      <form
+        className="row g-3"
+        onSubmit={(e) => submit(e)}
+      >
         <div className="col-md-4">
           <label
             htmlFor="validationServer01"
@@ -40,9 +67,10 @@ const AddReservation = () => {
 
           <select
             aria-label=".form-select-lg example"
-            value={cityName}
-            onChange={(e) => setCityName(e.target.value)}
-            className={`mb-3 form-select-lg form-select ${cityName.trim() ? 'is-valid' : ''}`}
+            value={userReserve.cityName}
+            id="cityName"
+            onChange={(e) => handleUserReserve(e)}
+            className={`mb-3 form-select-lg form-select ${userReserve.cityName.trim() ? 'is-valid' : ''}`}
           >
             <option selected>Select a City</option>
             <option value="1">One</option>
@@ -53,9 +81,10 @@ const AddReservation = () => {
           <input
             type="date"
             min={todayDate}
-            className={`form-control ${date >= todayDate ? 'is-valid' : ''}`}
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+            id="date"
+            className={`form-control ${userReserve.date >= todayDate ? 'is-valid' : ''}`}
+            value={userReserve.date}
+            onChange={(e) => handleUserReserve(e)}
             required
           />
 
