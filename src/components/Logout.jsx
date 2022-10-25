3f332e9765db344b5cf62f51d209c33c5c995a88
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-25 17:16:10 -0500

    Create logout component

---
+++ src/components/Logout.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import Button from '@mui/material/Button';
+import { LoginContext } from '../auth/UserAuth';
+
+function Logout() {
+  // eslint-disable-next-line
+  const { isAuth, setAuth } = useContext(LoginContext);
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:3000/logout';
+    const storageInfo = JSON.parse(localStorage.getItem('userInfo'));
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${storageInfo.token}`,
+      },
+    });
+    if (response.status === 200) {
+      setAuth(localStorage.setItem('isAuth', false));
+      window.location.href = '/login';
+    }
+    return response;
+  };
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={handleClick}
+    >
+      Logout
+    </Button>
+  );
+}
+
+export default Logout;
