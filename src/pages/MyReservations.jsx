2e15105654e0bf2d3f66f5d86f59aa542191a524
Daf <dafne.azzolina@gmail.com>
2022-10-27 18:38:01 -0300

    Fetch data for "My Reservations" component

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,29 +1,66 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { displayRooms, displayReservations } from '../redux/actionCreator';
+import { Container, CircularProgress } from '@mui/material';
+import { displayReservations } from '../redux/actionCreator';
 
-const MyReservations = () => {
+function MyReservations() {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations);
+  const rooms = useSelector((state) => state.rooms);
+
   useEffect(() => {
-    dispatch(displayRooms());
     dispatch(displayReservations());
   }, [dispatch]);
+
+  function container() {
+    return (
+      <Container maxWidth={false}>
+        <CircularProgress />
+      </Container>
+    );
+  }
   return (
     <>
-      {!reservations.length ? (<div>Loading...</div>)
-        : (
-          <>
+      {!reservations.length ? (
+        container()
+      ) : (
+        <Container maxWidth={false}>
           <h1>My Reservations</h1>
-              {reservations.map((reservation) => (
-                <React.Fragment key={reservation.id}>
-                <div>{reservation.city}</div>
-                </React.Fragment>
-              ))}
-          </>
-        )}
+          {reservations.map((reservation) => (
+            <React.Fragment key={reservation.id}>
+              <div style={{ border: '1px solid black' }}>
+                <h2>
+                  Reservation #
+                  {reservation.id}
+                </h2>
+                {rooms.map((room) => {
+                  const singleRoom = room;
+                  if (reservation.room_id === singleRoom.id) {
+                    return (
+                      <h4>
+                        Room:
+                        {singleRoom.name}
+                      </h4>
+                    );
+                  }
+                  return null;
+                },
+                )}
+                <h4>
+                  City:
+                  {reservation.city}
+                </h4>
+                <h4>
+                  Date:
+                  {reservation.date}
+                </h4>
+              </div>
+            </React.Fragment>
+          ))}
+        </Container>
+      )}
     </>
-  )
+  );
 }
 
 export default MyReservations;
