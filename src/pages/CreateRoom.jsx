f6b147cc32b2c598fca804df35c0b90fe0892158
dqarias <dino.quispea@gmail.com>
2022-10-26 23:35:30 -0500

    Add a page to navigate for adding a room

---
+++ src/pages/CreateRoom.jsx
@@ -0,0 +1,11 @@
+import Toolbar from '@mui/material/Toolbar';
+import AddRoom from '../components/AddRoom';
+
+const CreateRoom = () => (
+  <>
+    <Toolbar />
+    <AddRoom />
+  </>
+);
+
+export default CreateRoom;
