d4289086f301b4cf1714f237fbd51be9b04b8125
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-28 15:35:44 -0500

    Create details page table

---
+++ src/components/DetailsTable.jsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, price) {
+  return {
+    name, price,
+  };
+}
+
+const financeFee = Math.round(Math.random() * 1000);
+const optionFee = Math.round(Math.random() * 10000);
+const totalFee = financeFee + optionFee;
+const rows = [
+  createData('Finance fee', `${financeFee}`),
+  createData('Option to purchase fee', `${optionFee}`),
+  createData('Total ammount payable', `${totalFee}`),
+  createData('Duration'),
+];
+
+export default function DetailsTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 200 }} aria-label="customized table">
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.price}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
