dc2fefd5f5ea00f798ea100e147a92e7befe8ab8
dqarias <dino.quispea@gmail.com>
2022-10-28 07:15:52 -0500

    Add form for creating a new room

--- src/components/AddRoom.jsx
+++ src/components/AddRoom.jsx
@@ -1,31 +1,59 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-
+import React, {useEffect, useState} from 'react'
 
 const AddRoom = () => {
+const [roomArray, setRoomArray] = useState({
+  name: '',
+  description: '',
+  photo: ''
+})
+
+const handleInputChange = (e) => {
+  setRoomArray(
+    {
+      ...roomArray,
+      [e.target.name]: e.target.value,
+    }
+  )
+}
+
+console.log(roomArray)
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const newRoom = {
+    name: roomArray.name,
+    description: roomArray.description,
+    photo: roomArray.photo
+  }
+}
+
   return (
-    <div>
+      <div>
         <div>AddRoom</div>
-        <form action="">
-        <TextField 
-        id="standard-basic" 
-        label="Room Name" 
-        variant="standard" />
-
-        <TextField 
-        id="standard-basic" 
-        label="Room Description" 
-        variant="standard" />
-        
-        <TextField 
-        id="standard-basic" 
-        label="Photo URL" 
-        variant="standard" />
+        <form>
+          <input 
+          type="text"
+          name="name"
+          value={roomArray.name}
+          onChange={(e)=>handleInputChange(e)} />
+          <textarea 
+          name="description" 
+          id=""
+          value={roomArray.description}
+          onChange={(e)=>handleInputChange(e)} 
+          cols="30" 
+          rows="10">
+          </textarea>
+          <input 
+          type="text"
+          name="photo"
+          value={roomArray.photo}
+          onChange={(e)=>handleInputChange(e)} />
+          <button
+          type="button"
+          onClick={handleSubmit}>Add Room</button>
         </form>
-     
-
-    </div>
-    
+      </div>
   )
 }
 
