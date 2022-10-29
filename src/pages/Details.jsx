b02aa9ce88424dd4e69683c1db98f76bf3fa6f64
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-29 18:48:13 -0500

    Get and pass down props, validate props

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,23 @@
 import { Container } from '@mui/material';
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import DetailsGrid from '../components/DetailsGrid';
 
-const DetailsPage = () => (
+const DetailsPage = ({ name, description, photo }) => (
   <Container maxWidth={false} disableGutters>
-    <DetailsGrid />
+    <DetailsGrid
+      name={name}
+      description={description}
+      photo={photo}
+    />
   </Container>
 
 );
+
+DetailsPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  photo: PropTypes.string.isRequired,
+};
 
 export default DetailsPage;
