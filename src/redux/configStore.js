a914199bded63f16192219c18ce5583e931c09ac
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-31 16:53:42 -0500

    Add new state

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { roomsReducer, reservationsReducer } from './actionCreator';
+import { roomsReducer, reservationsReducer, idReducer } from './actionCreator';
 
 const store = configureStore({
   reducer: {
     rooms: roomsReducer,
     reservations: reservationsReducer,
+    roomId: idReducer
   },
 });
 
