65313f0ca236764fb695c86fc64ee2ae29abd9c6
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-31 16:52:58 -0500

    Get global state

--- src/components/ReserveRoom.jsx
+++ src/components/ReserveRoom.jsx
@@ -1,46 +1,48 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
 
 
 export default function ReserveRoom() {
 
-const [info, setInfo] = useState({
-  city: '',
-  date: '',
-});
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-};
-
-const getFormInfo = (e) => {
-  setInfo(
-    {
-      ...info,
-      [e.target.name]: e.target.value,
-    },
-  );
-};
-
-const todayDate = new Date().toISOString().slice(0, 10);
-
-const url = 'http://localhost:3000/rooms';
-
-const postReservation = async () => {
-  const storageInfo = JSON.parse(localStorage.getItem('userInfo'));
-  await fetch(`${url}/${roomId}/reservations`, {
-    method: 'POST',
-    body: JSON.stringify({
-      city: info.city,
-      date: info.date,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `${storageInfo.token}`,
-    },
+  const roomId = useSelector((state) => state.roomId)
+  const [info, setInfo] = useState({
+    city: '',
+    date: '',
   });
-  window.location.href = '/';
-};
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const getFormInfo = (e) => {
+    setInfo(
+      {
+        ...info,
+        [e.target.name]: e.target.value,
+      },
+    );
+  };
+
+  const todayDate = new Date().toISOString().slice(0, 10);
+
+  const url = 'http://localhost:3000/rooms';
+
+  const postReservation = async () => {
+    const storageInfo = JSON.parse(localStorage.getItem('userInfo'));
+    await fetch(`${url}/${roomId}/reservations`, {
+      method: 'POST',
+      body: JSON.stringify({
+        city: info.city,
+        date: info.date,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${storageInfo.token}`,
+      },
+    });
+    window.location.href = '/';
+  };
+
   return (
     <div className="reservation">
       <h1 className="form-title">Reserve room</h1>
